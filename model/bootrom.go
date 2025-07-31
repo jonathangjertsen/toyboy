23510716f10894de431fcdd97a7b003339976628
Jonathan Gjertsen <jonath.re@gmail.com>
2025-07-31 15:13:55 +0200

    Yoink BootROM memory region

--- model/bootrom.go
+++ model/bootrom.go
@@ -1,27 +1,18 @@
 package model
 
 type BootROMLock struct {
-	MemoryRegion
-
 	BootOff bool
 	OnLock  func()
 }
 
 func NewBootROMLock(clock *ClockRT) *BootROMLock {
-	return &BootROMLock{
-		MemoryRegion: NewMemoryRegion(clock, AddrBootROMLock, 0x0001),
-	}
-}
-
-func (brl *BootROMLock) Read(addr Addr) Data8 {
-	return brl.MemoryRegion.Read(addr)
+	return &BootROMLock{}
 }
 
 func (brl *BootROMLock) Write(addr Addr, v Data8) {
 	if brl.BootOff {
 		return
 	}
-	brl.MemoryRegion.Write(addr, v)
 	if v&1 == 1 {
 		brl.Lock()
 	}
